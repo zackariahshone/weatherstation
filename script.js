9281781156de5c35b31f85780a67ca5ebe186982
Zack Shone <zackariahshone@gmail.com>
2020-07-15 22:41:56 -0700

    massive amoutn of changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let x = 1;
-let y = 3;
 let nCity;
+
+//const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
 let date = new Date();
 console.log(date);
@@ -14,7 +15,9 @@ let city = '';
 //     $('.prevbuttons').prepend(nCity);
 //     console.log(city);
 // });
-
+function convert(tempk){
+  return  tempf = Math.floor((tempk - 273.15) * 9/5 + 32);
+}
 $('#show').click(function () {
     
     city = $('#city').val();
@@ -32,14 +35,50 @@ $('#show').click(function () {
         })
         .then(function (response) {
             console.log(response);
+           const cityLon = response.coord.lon;
+           const cityLat = response.coord.lat;
+           const iconcode = response.weather[0].icon;
+
+           const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+           //(255.372K − 273.15) × 9/5 + 32
+           const tempk =response.main.temp;
+            const tempf = convert(tempk);
+            console.log("FARENHEIRT: " + tempf);
+
             $('.city-date').html('<h1>' + response.name + '</h1>');
-            $('.citytemp').html('<h2>' + response.main.temp + '</h2>');
-            $('.cityhumidity').html('<h2>' + response.main.humidity + '</h2>');
-            $('.citywind').html('<h2>' + response.wind.speed + '</h2>');
-            
-        })
-
-
+            $('.citytemp').html('<h2> Temp: ' + tempf + '</h2>');
+            $('.cityhumidity').html('<h2> Humidity:' + response.main.humidity + '</h2>');
+            $('.citywind').html('<h2> Wind Speed: ' + response.wind.speed + '</h2>');
+            $('#weatherIcon').attr('src',iconurl);
+            //FOR UV INDEX
+            const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+cityLat+"&lon=" + cityLon;
+            $.ajax({
+                url: uvUrl,
+                method: "GET"
+            })
+            .then(function(response){
+               let uIndex = response.value;
+                console.log("UV Data: "+response.value);
+                $("#uv").text(response.value);
+                
+                if(uIndex > 9){
+                    $('#uv').removeClass();
+                    $('#uv').addClass('highUv');
+                }
+                 if(uIndex > 5 && uIndex <= 8){
+                    $('#uv').removeClass();
+                     $('#uv').addClass('medUv');
+                 }
+                if(uIndex < 4){
+                    $('#uv').removeClass();
+                    $('#uv').addClass('lowUv');
+                }
+               
+            });
+        
+        });
+        
+      
 
    
     const queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
@@ -48,80 +87,43 @@ $('#show').click(function () {
             method: "GET"
         })
         .then(function (response) {
-            console.log(queryURLFive);
-            console.log(response);
-           $('.citydetails').html('<h2>' + response.city.name+' Four Day Forcast </h2>');
-        
-            for (let i = 0; i < 3; i++) {
-                console.log("this is Y: " + y);
+            // console.log(queryURLFive);
+            // console.log(response);
+           $('.citydetails').html('<h2>' + response.city.name+' Five Day Forcast </h2>');
+           let y = 2;
+            for (let i = 0; i < 5; i++) {
+                // console.log("this is Y: " + y);
                 
+               const maxF = convert(response.list[y].main.temp_max);
+               const minF = convert(response.list[y].main.temp_min);
+               
+
+
                 
                 $(".date"+x).html(response.list[y].dt_txt);
 
-                console.log(response.list[y].dt_txt);
-
+                // console.log(response.list[y].dt_txt);
+                const maxt = convert()
                 
-                $(".temp" + x).text("High: " + response.list[y].main.temp_max);
+                $(".temp" + x).text("High: " + maxF);
                 
-                $(".wind" + x).text('Low: ' + response.list[y].main.temp_min);
+                $(".wind" + x).text('Low: ' + minF);
                 
                 $(".tempF" + x).text("Humidity: " + response.list[y].main.humidity);
 
-                y = y + 4;
+                y = y + 8;
                 x++;
-                console.log("this is Y: " + y);
-                console.log("x needs to be reset also"+ x );
+                // console.log("this is Y: " + y);
+                // console.log("x needs to be reset also"+ x );
                 
             }
-            if(y >= 15){
-                y = 4;
+            if(y >= 40){
+                y = 2;
                 x = 1;
-                console.log("Y is reset y= "+y);
+                // console.log("Y is reset y= "+y);
             }
         });
+
+
 });
 
-//)};
-
-
-
-
-//  const APIKey = "6af3e9cb79cfccec5f936552226ac51d";
-
-//  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=" + APIKey;
-//  //   // Here we run our AJAX call to the OpenWeatherMap API
-//    $.ajax({
-//          url: queryURL,
-//          method: "GET"
-//        })
-//        // We store all of the retrieved data inside of an object called "response"
-//          .then(function(response) {
-//                console.log(queryURL);
-//                // Log the resulting object
-//                console.log(response.city.name);
-
-//          });
-
-
-
-// Transfer content to HTML
-
-//         const br = $('<br>');
-//      for (let i = 0; i < 3; i++) {
-//         console.log("this is Y: " + y);
-//         $(".city"+x).prepend("<h1>" + response.city.name + " Weather Details"+"</h1>"+
-//         "<h3>"+ response.list[y].dt_txt +"</h3>");
-
-//         console.log(response.list[y].dt_txt);
-
-//         $('.wind' + x).prepend(br); 
-//         $(".wind" +x).prepend("High: " + response.list[y].main.temp_max);
-//         $('.wind' + x).prepend(br);
-//         $(".temp" + x).prepend('Low: ' + response.list[y].main.temp_min);
-//         $('.wind' + x).prepend(br);
-//         $(".tempF" + x).prepend("Humidity: " + response.list[y].main.humidity); 
-
-//         y+=4;
-//         x++;
-//         console.log("this is Y: " + y);
-//   }
